f8d076b3c36f279644b21b141e194cd55be25602
培 唐 <tp@Tang.local>
2017-07-12 16:35:07 +0800

    first commit

---
+++ FoodPin/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  FoodPin
+//
+//  Created by 培 唐 on 2017/7/11.
+//  Copyright © 2017年 唐培. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FoodPin/GlobalVars.swift
@@ -0,0 +1,9 @@
+//
+//  GlobalVars.swift
+//  FoodPin
+//
+//  Created by 培 唐 on 2017/7/11.
+//  Copyright © 2017年 唐培. All rights reserved.
+//
+
+import Foundation
