5f34976736400ed48ceb6665a7650c39f587a965
培 唐 <tp@Tang.local>
2017-07-14 17:49:37 +0800

    commit

---
+++ FoodPin/UIView+FrameChangeTool.h
@@ -0,0 +1,21 @@
+//
+//  UIView+FrameChangeTool.h
+//  Done
+//
+//  Created by 边智峰 on 16/9/15.
+//  Copyright (c) 2015年 Socap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FrameChangeTool)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+@end
